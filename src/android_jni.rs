5d89f9dba100dc931a0680d4e9930ffad2e9e7cf
Kenan Schaefkofer <kbitikofer@gmail.com>
2026-01-03 14:44:46 -0800

    Remove unused imports

--- src/android_jni.rs
+++ src/android_jni.rs
@@ -1,7 +1,6 @@
 use crate::android_frontend::AndroidFrontend;
 use crate::app_state::KeyState;
 use crate::input_map::{UiButton, UiKey};
-use crate::layout;
 
 #[cfg(all(target_os = "android", feature = "android"))]
 use crate::android_aaudio;
@@ -10,7 +9,7 @@ use crate::chord_wheel::{self, WheelDir8};
 use crate::touch::{PointerId, TouchEvent, TouchPhase};
 
 use jni::objects::{JClass, JIntArray, JShortArray};
-use jni::sys::{jboolean, jfloat, jint, jlong, jshort};
+use jni::sys::{jboolean, jfloat, jint, jlong};
 use jni::JNIEnv;
 
 macro_rules! frontend_mut_or_return {
